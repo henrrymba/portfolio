ef6bc673e05d6ff276f1a8ea8b393276b603c9a8
Henrry Muraña Bautista <henrrymba@icloud.com>
2025-12-19 20:07:51 +0100

    feat: Add Google Analytics and Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import Desktop from "./pages/Desktop";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Landing />} />
         <Route path="/portfolio" element={<Desktop />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 };
