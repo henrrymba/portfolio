306531b7b9dac8c56f2179dba960b5d93d60eb19
Henrry Muraña Bautista <henrrymba@icloud.com>
2025-12-19 16:43:41 +0100

    feat: add Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import "./i18n";
 import App from "./App.jsx";
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>
 );
